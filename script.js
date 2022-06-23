a197e4987a7c52b636d201efaa21781b3296d3bf
PavlePetrovic <pavlep275@gmail.com>
2022-06-23 13:35:37 +0200

    Update Best service section and add hover effect for Featured work

--- script.js
+++ script.js
@@ -1,98 +1,105 @@
 // Importujemo biblioteku - Animacija brojeva
 import { CountUp } from './counterJs/dist/countUp.min.js';
 
-/*                     DOM                  */
-// Mobile menu
-let body = document.querySelector('body')
-let hamburgerIcon = document.querySelector('.mobileMenuDiv')
-let mobileMenuItems = document.querySelector('.mobileMenuItems')
-// Card section
-let cardSection = document.querySelector('.whatWeDoSectionCards')
-let cardBox = document.querySelectorAll('.card')
-// Counter section
-let num1 = document.getElementById('number1')
-let num2 = document.getElementById('number2')
-let num3 = document.getElementById('number3')
-let num4 = document.getElementById('number4') 
+window.onload = function() {
 
-/*                  MOBILE MENU                 */
-// Hamburger icon
-hamburgerIcon.addEventListener('click', () => {
-    body.classList.toggle('stopScroll')
-    mobileMenuItems.classList.toggle('open')
-    
-    for(let i = 0; i < 3; i++){
-        hamburgerIcon.children[i].classList.toggle('open')
-    }
-})
-// Hamburger menu items
-mobileMenuItems.addEventListener('click', () => {
-    body.classList.toggle('stopScroll')
-    mobileMenuItems.classList.toggle('open')
+    /*                     DOM                  */
+    // Mobile menu
+    let body = document.querySelector('body')
+    let hamburgerIcon = document.querySelector('.mobileMenuDiv')
+    let mobileMenuItems = document.querySelector('.mobileMenuItems')
+    // Card section
+    let cardSection = document.querySelector('.whatWeDoSectionCards')
+    let cardBox = document.querySelectorAll('.card')
+    // Counter section
+    let num1 = document.getElementById('number1')
+    let num2 = document.getElementById('number2')
+    let num3 = document.getElementById('number3')
+    let num4 = document.getElementById('number4') 
 
-    for(let i = 0; i < 3; i++){
-        hamburgerIcon.children[i].classList.toggle('open')
-    }
-})
+    /*                  MOBILE MENU                 */
+    // Hamburger icon
+    hamburgerIcon.addEventListener('click', () => {
+        body.classList.toggle('stopScroll')
+        mobileMenuItems.classList.toggle('open')
+        
+        for(let i = 0; i < 3; i++){
+            hamburgerIcon.children[i].classList.toggle('open')
+        }
+    })
+    // Hamburger menu items
+    mobileMenuItems.addEventListener('click', () => {
+        body.classList.toggle('stopScroll')
+        mobileMenuItems.classList.toggle('open')
 
-/*                   CARD SECTION                  */
-cardBox.forEach( card => {
+        for(let i = 0; i < 3; i++){
+            hamburgerIcon.children[i].classList.toggle('open')
+        }
+    })
 
-    card.addEventListener('click', function(){
+    /*                   CARD SECTION                  */
+    cardBox.forEach( card => {
 
-        let card1 = this.parentElement.parentElement.children[0].children[0]
-        let card2 = this.parentElement.parentElement.children[0].children[1]
-        let card3 = this.parentElement.parentElement.children[1].children[0]
-        let card4 = this.parentElement.parentElement.children[1].children[1]
+        card.addEventListener('click', function() {
 
-        let allCards = [card1, card2, card3, card4]
+            let card1 = this.parentElement.parentElement.children[0].children[0]
+            let card2 = this.parentElement.parentElement.children[0].children[1]
+            let card3 = this.parentElement.parentElement.children[1].children[0]
+            let card4 = this.parentElement.parentElement.children[1].children[1]
 
-        allCards.forEach(card => {
-            // Kartica
-            card.classList.remove('clickedCard')
-            // Div u kome je slicica kartice
-            card.children[0].classList.remove('clickedCardImage')
-            // Span element - Linija
-            card.children[1].children[1].classList.remove('clickedSpan')
-            // Slicica u kartici - putanja
-            card.children[0].children[0].setAttribute('src', './img/computerWhite.png')
-        })
+            let allCards = [card1, card2, card3, card4]
 
-        let clickedCard = card
-        allCards.forEach(card => {
-            if(clickedCard === card){
-                 card.classList.toggle('clickedCard')
-                card.children[0].classList.toggle('clickedCardImage')
-                card.children[1].children[1].classList.toggle('clickedSpan')
+            allCards.forEach(card => {
+                // Kartica
+                card.classList.remove('clickedCard')
+                // Div u kome je slicica kartice
+                card.children[0].classList.remove('clickedCardImage')
+                // Span element - Linija
+                card.children[1].children[1].classList.remove('clickedSpan')
+                // Slicica u kartici - putanja
+                card.children[0].children[0].setAttribute('src', './img/computerWhite.png')
+            })
+
+            console.log(card);
+
+            if( card.classList.contains('clickedCard') ){
+                card.classList.removeAll()
+                card.children[0].classList.removeAll()
+                card.children[1].children[1].classList.removeAll()
+                card.children[0].children[0].setAttribute('src', './img/computerOrange.png')
+            } else{
+                card.classList.add('clickedCard')
+                card.children[0].classList.add('clickedCardImage')
+                card.children[1].children[1].classList.add('clickedSpan')
                 card.children[0].children[0].setAttribute('src', './img/computerOrange.png')
             }
+        
         })
+    } )
 
-    })
-} )
-
-/*                  COUNTER SECTION                 */
-// Animacija brojeva ispod "What we do" sekcije
-function counter(num) {
-    if(num == num1){
-        const countUp4 = new CountUp(num, 1580, { enableScrollSpy: true, useGrouping: false });
-        countUp4.handleScroll();
-    } else if(num == num2){
-        const countUp4 = new CountUp(num, 2850, { enableScrollSpy: true, useGrouping: false });
-        countUp4.handleScroll();
-    } else if(num == num3){
-        const countUp4 = new CountUp(num, 1500, { enableScrollSpy: true, useGrouping: false });
-        countUp4.handleScroll();
-    } else if(num == num4){
-        const countUp4 = new CountUp(num, 1430, { enableScrollSpy: true, useGrouping: false });
-        countUp4.handleScroll();
+    /*                  COUNTER SECTION                 */
+    // Animacija brojeva ispod "What we do" sekcije
+    function counter(num) {
+        if(num == num1){
+            const countUp4 = new CountUp(num, 1580, { enableScrollSpy: true, useGrouping: false });
+            countUp4.handleScroll();
+        } else if(num == num2){
+            const countUp4 = new CountUp(num, 2850, { enableScrollSpy: true, useGrouping: false });
+            countUp4.handleScroll();
+        } else if(num == num3){
+            const countUp4 = new CountUp(num, 1500, { enableScrollSpy: true, useGrouping: false });
+            countUp4.handleScroll();
+        } else if(num == num4){
+            const countUp4 = new CountUp(num, 1430, { enableScrollSpy: true, useGrouping: false });
+            countUp4.handleScroll();
+        }
     }
+
+    window.onload = counter(num1)
+    window.onload = counter(num2)
+    window.onload = counter(num3)
+    window.onload = counter(num4)
+
 }
-
-window.onload = counter(num1)
-window.onload = counter(num2)
-window.onload = counter(num3)
-window.onload = counter(num4)
-
 
 
